0bf98cbe0a798559614e7176f9d79cc3ccb1fab4
bensvarnum <bens.varnum@gmail.com>
2021-03-05 12:10:39 -0500

    login form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 import ChatFeed from './components/ChatFeed'
+import LoginForm from './components/LoginForm';
 
 const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />;
     
     return (
         <ChatEngine
         height="100vh"
         projectID = {process.env.React_App_Project_Id}
-        userName= {process.env.React_App_User_Name}
-        userSecret= {process.env.React_App_User_Secret}
+        userName= {localStorage.getItem('username')}
+        userSecret= {localStorage.getItem('password')}
         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps }/>}
+        onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
         />
     )
 }
